f9d29e94f2c4adf09944c8a2bfeb461b9317912c
shubhanvithaa <shubhanvithaa@gmail.com>
2025-02-11 23:45:35 +0530

    dupliactes

--- duplicates.py
+++ duplicates.py
@@ -1,5 +1,6 @@
 #finding duplicates.-- Time complexity o(n^2)
 
+
 nums =  list(map(int, input("Enter numbers separated by space: ").split()))
 count = 0
 length = len(nums)
@@ -15,7 +16,15 @@ if(count == 0):
     print("False")
 else:
     print("True")
+#solution 2
 
+nums.sort()
+length = len(nums)
+for i in range(1,length):
+    if nums[i] == nums[i-1]:
+        print("True")
+        break
+    print("False")       
 
 
 
