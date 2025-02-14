dccce971e9c38d7ac48d8cc1feae06bf27681ba1
shubhanvithaa <shubhanvithaa@gmail.com>
2025-02-14 23:40:39 +0530

    kth-largest.py

--- kth-largest.py
+++ kth-largest.py
@@ -2,3 +2,10 @@ import heapq
 nums = [3,2,3,1,2,4,5,5,6] 
 k = 4
 print( heapq.nlargest(k, nums)[-1])
+
+#or through sorting
+
+nums = [3,2,3,1,2,4,5,5,6] 
+nums.sort(reverse = True)
+k = 4
+print(nums[k-1])
